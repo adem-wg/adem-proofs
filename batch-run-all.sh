b5465b8a56712d86391936ab83e3d896f7e87936
Felix Linker <linkerfelix@gmail.com>
2023-08-30 16:16:50 +0200

    Update script

--- batch-run-all.sh
+++ batch-run-all.sh
@@ -1,1 +1,13 @@
-tamarin-prover-release +RTS -N10 -RTS --output=proofs.spthy --prove adem.spthy > proofs.log 2>&1 &
+tamarin-prover-release +RTS -N10 -RTS --output=proofs.spthy \
+  --prove=TLSKeyShareRecursion \
+  --prove=TLSKeyLeakRecursion \
+  --prove=WrongCAKeyUseImpliesCompromise \
+  --prove=CanObtainRootKey \
+  --prove=VerifiedAuthorityOrigin \
+  --prove=AuthenticEmblem \
+  --prove=CanDisupte \
+  --prove=CAAccountability \
+  --prove=AuthorityAccountability \
+  --prove=PPAccountability \
+  --prove=RootKeyUse \
+  adem.spthy > proofs.log 2>&1 &
